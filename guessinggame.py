203ece7dc96e9b66f7c2cbfbfaac2c5edbde8235
#codename$ <95187806+sakib4535@users.noreply.github.com>
2022-03-24 00:34:02 +0600

    Add files via upload

---
+++ guessinggame.py
@@ -0,0 +1,25 @@
+import random
+
+highest = 15
+answer = random.randint(1,highest)
+guess = 0
+print(answer)
+
+while guess != answer:
+    print("Please Get the Correct $$Value$$ of the Coin Between 1 - {}: ".format(highest))
+    guess = int(input())
+
+    if guess == answer:
+        print("You Guessed it Right First Time, You Have the NFT DATABASE Now in C:/Users?####!!")
+        break
+    else:
+        if guess < answer:
+            print("Please Guess the Higher Value of the Coin")
+        else:
+            print("Please Guess the Lower value of the Coin")
+        guess = int(input())
+        if guess == answer:
+            print("Well Done, You Got the Right Valuation - $$####$$")
+        else:
+            print("Sorry, You Failed and Get Nothing @@")
+
